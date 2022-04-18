6daee85210001c322de4452e7f5210ce62756fc0
Avik <aviksingha2017@gmail.com>
2022-04-18 12:45:55 +0530

    Initial project setup

---
+++ good_news/good_news/frontend/src/constants.js
@@ -0,0 +1,14 @@
+﻿const ENDPOINT = {
+  LIST: "/api/list",
+  GRID: "/api/grid",
+};
+
+const ERROR_MESSAGE = {
+  LIST_DELETE: "Request to delete list item failed:",
+  LIST_ADD: "Request to add list item failed:",
+  LIST_GET: "Request to get list items failed:",
+  LIST_EMPTY_MESSAGE: "Please enter a valid message",
+  GRID_GET: "Request to get grid text failed:",
+};
+
+export {ENDPOINT, ERROR_MESSAGE};
